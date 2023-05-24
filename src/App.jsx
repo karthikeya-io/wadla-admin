3b0f62e84b9da927c9d27059f776bd9eabb25b2c
karthikeya-io <karthikeyabethu01@gmail.com>
2023-05-24 13:27:36 +0530

    signup page for admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Admin from './pages/Admin';
-import BannerUpdate from './pages/BannerUpdate';
-import Login from './pages/Login';
+import Admin from './pages/admin/Admin';
+import BannerUpdate from './pages/admin/BannerUpdate';
+import Login from './pages/admin/Login';
+import Signup from './pages/admin/Signup';
 
 
 function App() {
@@ -10,10 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="about" element={<h1>About</h1>} />
+        <Route path="signup" element={<Signup/>} />
         <Route path="login" element={<Login/>} />
-        <Route path="admin" element={<Admin/>} />
-        <Route path="admin/banner" element={<BannerUpdate/>} />
+        <Route path="dashboard" element={<Admin/>} />
+        <Route path="update/banner" element={<BannerUpdate/>} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Router>
