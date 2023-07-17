a5fbc0741c86c5386e8cf690a614ca0f5db103ec
karthikeya-io <karthikeyabethu01@gmail.com>
2023-07-18 00:56:56 +0530

    added route to registration page

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Speakers from "./pages/Speakers";
 import Events from "./pages/Events";
-
-
+import Registrations from "./pages/Registrations";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Events/>} />
+        <Route path="/" element={<Events />} />
         <Route path="/event/:eventId/:eventName">
-          <Route index element={<Dashboard/>} />
+          <Route index element={<Dashboard />} />
+          <Route path="registrations" element={<Registrations />} />
           <Route path="speakers" element={<Speakers />} />
           <Route path="banner" element={<BannerUpdate />} />
         </Route>
-        <Route path="update" >
-          <Route index element={<Dashboard/>} />
+        <Route path="update">
+          <Route index element={<Dashboard />} />
           <Route path="banner" element={<BannerUpdate />} />
           <Route path="speakers" element={<Speakers />} />
         </Route>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,37 @@ import { Link, useParams } from "react-router-dom";
 import classes from "../css/Dashboard.module.css";
 import { useEffect } from "react";
 import Welcome from "../components/Welcome";
-
+import { Button } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Dashboard = () => {
   const { eventId, eventName } = useParams();
   return (
     <div className={classes.body}>
       <Welcome message={`Welcome to ${eventName} Dashboard!`} />
+      <Link
+        to="registrations"
+        style={{
+          display: "flex",
+          margin: "auto",
+          textDecoration: "none",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            display: "flex",
+            margin: "auto",
+            marginTop: "20px",
+            backgroundColor: "#3f51b5",
+            color: "#fff",
+          }}
+        >
+          View Registrations
+          <ArrowForwardIosIcon />
+        </Button>
+      </Link>
       <div className={`${classes.continueLearning} ${classes.addGrid}`}>
         <Link to="/update/banner">
           <div className={classes.add}>
